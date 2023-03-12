ab0e260404aea40ddd0d4baf9759349774cccc50
Nattaphat <nattaphat.ngar@kmutt.ac.th>
2023-03-12 20:39:06 +0700

    'version1'

--- ABC/src/App.vue
+++ ABC/src/App.vue
@@ -1,47 +1,35 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import TimerComponent from './components/TimerComponent.vue';
+import CustomTime from './components/CustomTime.vue';
+import { ref,onMounted } from 'vue';
+
+const customized_min = ref(25) // initial minutes
+const customized_sec = ref(0) // initial seconds
+
+const timeSelected = (data) =>{
+    console.log(`Min:${data.min},Sec:${data.sec}`)
+        customized_min.value = data.min
+        customized_sec.value = data.sec
+    
+}
+onMounted(()=>{
+    document.title = "Tomadoro Timer"
+})
+
 </script>
-
+ 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+        <p class="text-center mt-4 text-xl font-bold text-red-400">Timer Component</p>
+        <div class="flex justify-center">
+            <div>
+                <TimerComponent :minutes="customized_min" :seconds="customized_sec"></TimerComponent>
+                <div class="flex justify-center mt-5">
+                   <p class="font-bold text-xl text-red-400 text-center ">Customize Component</p>
+                </div>
+               <CustomTime @customizedTime="timeSelected" class="mt-5" />
+            </div>
+        </div>
+        <p class="flex justify-center mt-10">Disclaimer: All alert sounds featured in this Web Application are sourced from Windows 11 Media, which is owned by &copy; Microsoft Corporation.</p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ ABC/src/components/CustomTime.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref,defineEmits, computed } from 'vue';
+const custom_min = ref()
+const custom_sec = ref()
+const emits = defineEmits(['customizedTime']) // define emits
+
+const emitsClick = () => { // define fucnion onClick button
+    // emits values in object term, when user clicked the button a values will emits to parent component (App.vue)
+    emits('customizedTime',{min:custom_min.value,sec:custom_sec.value})
+}
+
+
+</script>
+ 
+<template>
+<div>
+    <div class="custom-time inline-flex gap-x-4 mt-5 ml-10">
+            Minutes:<input type="number" min="0" max="99" maxlength="2" placeholder="Min" v-model="custom_min" class="input input-bordered input-warning w-20 max-w-xs"/>
+            Sec:<input type="number" min="0" max="59" maxlength="2" placeholder="Sec" v-model="custom_sec" class="input input-bordered input-warning w-20 max-w-xs" />
+            <button type="submit" @click="emitsClick" class="border-2 border-red-300 p-2 w-12 rounded-xl">Set</button>
+    </div>
+
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
